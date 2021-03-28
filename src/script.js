f66b41dcb59bcfd6174d30c3c0c8f5304d16f904
Michael Winser <mike.winser@gmail.com>
2021-03-28 14:17:50 -0700

    version 1

--- src/script.js
+++ src/script.js
@@ -1,21 +1,32 @@
-//window.focus()
+window.focus()
 let output = document.querySelector('#message')
 let gift = document.querySelector('#gift')
 let cake = document.querySelector('#cake')
+let flame = document.querySelector('#flame')
 let codeLog = []
-let konami = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft","ArrowRight","ArrowLeft","ArrowRight","b","a","b","a"]
+let konami = ["ArrowUp","ArrowUp","ArrowLeft","ArrowDown","ArrowRight","a","b"]
+
+function blowCandles() {
+  flame.classList.add('flicker')
+  setTimeout(()=>{
+    flame.classList.remove('flicker')
+  },1500)
+}
+
+cake.addEventListener('click', blowCandles)
+
 window.addEventListener('keyup', (e)=>{
   codeLog.push(e.key)
   if (codeLog.length>konami.length) {
     codeLog.shift()
   }
   if (codeLog.join('')===konami.join('')){
-    output.innerHTML = "KONAMI ENTERED!"
+    output.innerHTML = "HAPPY BIRTHDAY!"
     output.classList.add('konami')
     gift.classList.add('hide')
     cake.classList.remove('hide')
   } else {
-    output.innerHTML = "Open your gift! (press up, up, down, down, left, right, left, right, b, a, b, a.)"
+    output.innerHTML = "Use your cheat code to open your gift!"
     output.classList.remove('konami')
     gift.classList.remove('hide')
     cake.classList.add('hide')
